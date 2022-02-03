5fd211ed3ef73c9e4f525ef48387b6101bc0f1ea
heisenferg <heisenferg@gmail.com>
2022-02-03 10:51:05 +0100

    2 Calculo minimos

--- app/src/main/java/com/example/irpfcalcferfer/MainActivity.java
+++ app/src/main/java/com/example/irpfcalcferfer/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.irpfcalcferfer;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -25,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     public static Double baseIRPF;
     public static final int rendimientoTrabajo=2000;
+    public static Double salario;
+    public static Double baseImponible;
     public static int numeroHijos;
     public static int dia;
     public static int mes;
     public static int anio;
+    public static int minimoPersonal;
     public static int minimoFamiliar;
+    public static int eurosPorHijo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,56 +51,81 @@ public class MainActivity extends AppCompatActivity {
         botonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                calcularseguridadSocial();
+                calcularMinPersonal();
                 calcularBaseImponible();
+                calcularDescendientes();
+                calcularMinimoFamiliar();
+
             }
         });
 
     }
 
-    public void calcularBaseImponible(){
-        Double salario= Double.parseDouble((String) salarioBruto.getText());
+    public void calcularseguridadSocial(){
+        salario= Double.parseDouble((String) salarioBruto.getText());
         baseIRPF= salario * 0.0635;
     }
 
-    public void calcularMinFamiliar(){
+    public void calcularBaseImponible(){
+        baseImponible = salario - rendimientoTrabajo - baseIRPF;
+    }
+
+    public void calcularDescendientes(){
         numeroHijos = Integer.parseInt(String.valueOf(nHijos.getText()));
 
         if (numeroHijos==0){
-
+            eurosPorHijo=0;
+        }
+        if (numeroHijos==1){
+            eurosPorHijo=2400;
+        }
+        if (numeroHijos==2){
+            eurosPorHijo=2700+2400;
+        }
+        if (numeroHijos==3){
+            eurosPorHijo=4000+2400+2700;
+        }
+        if (numeroHijos>4){
+            eurosPorHijo=4500+4000+2400+2700;
         }
     }
 
 
-    public void calcularEdad(){
+    public void calcularMinPersonal(){
         Calendar ahora = Calendar.getInstance();
         int diaHoy = ahora.get(Calendar.DAY_OF_MONTH);
         int mesHoy = ahora.get(Calendar.MONTH);
         int anioHoy = ahora.get(Calendar.YEAR);
 
         if (anioHoy-anio <65){
-            minimoFamiliar=5550;
+            minimoPersonal =5550;
         }
         if (anioHoy-anio>65 && anioHoy-anio<75){
-            minimoFamiliar=6468;
+            minimoPersonal =6468;
         }
         if (anioHoy-anio>=75){
-            minimoFamiliar=7590;
+            minimoPersonal =7590;
         }
         if (anioHoy-anio==65){
             if (mesHoy-mes>0){
-                minimoFamiliar=6468;
+                minimoPersonal =6468;
             }
             if (mesHoy-mes==0){
                 if (diaHoy-dia>0){
-                    minimoFamiliar=6468;
+                    minimoPersonal =6468;
                 } else {
-                    minimoFamiliar=5550;
+                    minimoPersonal =5550;
                 }
             } else {
-                minimoFamiliar=5550;
+                minimoPersonal =5550;
             }
         }
 
+    }
+
+    public void calcularMinimoFamiliar(){
+        minimoFamiliar = minimoPersonal+eurosPorHijo;
     }
 
 
