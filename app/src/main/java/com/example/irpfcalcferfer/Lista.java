8fddaa6b6712064cf51f5f141aa06a9ca41c5fd2
Fernando Fernández <heisenferg@gmail.com>
2022-02-03 21:26:07 +0100

    5 - Casa Calculos tramos A Problema con tramo       tramoD = baseImpon-7750;             baseImpon-=tramoD;             tramoD = tramoD*0.24;
                Difernecia mal hecha

--- app/src/main/java/com/example/irpfcalcferfer/Lista.java
+++ app/src/main/java/com/example/irpfcalcferfer/Lista.java
@@ -2,6 +2,7 @@ package com.example.irpfcalcferfer;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
@@ -16,10 +17,10 @@ public class Lista extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista);
 
-
+calcularTramosBaseImponible();
     }
 
-    MainActivity.minimoFamiliar;
+  //  MainActivity.minimoFamiliar;
 
     Double baseImpon = MainActivity.baseImponible;
     Double tramoA;
@@ -34,24 +35,27 @@ public class Lista extends AppCompatActivity {
             baseImpon-=tramoA;
             tramoA = tramoA*0.45;
         }
-        if (baseImpon<60000 && baseImpon>=35200){
+
+        if (baseImpon<=60000 && baseImpon>=35200){
             tramoB = baseImpon-24800;
             baseImpon-=tramoB;
             tramoB = tramoB*0.37;
         }
-        if (baseImpon<35200 && baseImpon>=20200){
+        if (baseImpon<=35200 && baseImpon>=20200){
             tramoC = baseImpon-15000;
             baseImpon-=tramoC;
             tramoC = tramoC*0.30;
         }
-        if (baseImpon<20200 && baseImpon>=12450){
+        if (baseImpon<=20200 && baseImpon>=12450){
             tramoD = baseImpon-7750;
             baseImpon-=tramoD;
             tramoD = tramoD*0.24;
         }
-        if (baseImpon<12450){
+        if (baseImpon<=12450){
             tramoE = baseImpon*0.19;
         }
 
+        Log.d("TRAMOS", "A: " + tramoA + "B: " + tramoB +
+                "C: " + tramoC + "D: " + tramoD + "E: " +tramoE);
     }
 }
