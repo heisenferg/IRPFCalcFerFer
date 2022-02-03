4665452ebd59cd1f535ac4411cc36dd4bee70b32
heisenferg <heisenferg@gmail.com>
2022-02-03 10:14:58 +0100

    1 Empezando

---
+++ app/src/main/java/com/example/irpfcalcferfer/Calendario.java
@@ -0,0 +1,33 @@
+package com.example.irpfcalcferfer;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Calendario extends AppCompatActivity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        CalendarView calendarView = findViewById(R.id.calendario);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    MainActivity.dia = dayOfMonth;
+                    MainActivity.mes = month;
+                    MainActivity.anio = year;
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/irpfcalcferfer/MainActivity.java
+++ app/src/main/java/com/example/irpfcalcferfer/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.irpfcalcferfer;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     TextView fechaNacimiento;
@@ -14,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
     Button botonFecha;
     Button botonCalcular;
 
-    String baseIRPF;
-
+    public static Double baseIRPF;
+    public static final int rendimientoTrabajo=2000;
+    public static int numeroHijos;
+    public static int dia;
+    public static int mes;
+    public static int anio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
         botonFecha = findViewById(R.id.buttonFecha);
         botonCalcular = findViewById(R.id.buttonCalcular);
 
+        botonFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), Calendario.class);
+                    startActivity(intent);
+            }
+        });
+
         botonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,11 +59,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcularBaseImponible(){
-        Double baseImponible;
         Double salario= Double.parseDouble((String) salarioBruto.getText());
-        baseImponible= salario * 0.0635;
-        baseIRPF = String.valueOf(baseImponible);
+        baseIRPF= salario * 0.0635;
     }
+
+    public void calcularMinFamiliar(){
+        numeroHijos = Integer.parseInt(String.valueOf(nHijos.getText()));
+
+        if (numeroHijos==0){
+
+        }
+    }
+
+
+    public void calcularEdad(){
+        Calendar ahora = Calendar.getInstance();
+        int diaHoy = ahora.get(Calendar.DAY_OF_MONTH);
+        int mesHoy = ahora.get(Calendar.MONTH);
+        int anioHoy = ahora.get(Calendar.YEAR);
+
+
+
+    }
+
 
 
 }
